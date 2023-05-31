7e7311f2add29123fd63d2aa7300ee3a7cff0756
Nirali Rathod <59702795+Niraaaliii@users.noreply.github.com>
2023-05-31 13:18:17 -0500

    deleting

--- README.md
+++
@@ -1,3 +0,0 @@
-# To-Do-Project
-Simple To-Do List Project made with Java using Spring boot framework. 
-For the Database -> MySql
--- src/main/java/com/project_to_do/to_do_resources/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project_to_do.to_do_resources;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ToDoRepository extends CrudRepository<ToDo, Integer> {
-
-}
